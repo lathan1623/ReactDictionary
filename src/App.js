0065f37bac21e55807b20858e246047cc6c80c1d
lathan1623 <lathan1623@me.com>
2020-03-07 19:47:54 -0600

    changed comment

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import Modal from './Components/Modal';
 
 const API_KEY = '3110d706-0b47-4ffa-8b16-ed33de45f0ef';
 /**
- * Main App Class....
+ * Main App Class
  */
 class App extends React.Component {
   /**
-   * Initializes default states!!!
+   * Initializes default states
    */
   state = {
     show: false,
